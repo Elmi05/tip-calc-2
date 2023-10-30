72b3c2ef1e7eed88257a03abc50659da5d009770
Elmi05 <karshe0548@gmail.com>
2023-10-30 19:40:30 +0300

    done

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("hello");
+
+function calcTip(bill) {
+    let tip;
+  
+    if (bill >= 90 && bill <= 500) {
+      tip = bill * 0.15;
+    } else if (bill > 500) {
+      tip = bill * 0.2;
+    }
+  
+    return tip;
+  }
+  
+  let bill = 680;
+  let tip = calcTip(bill);
+  console.log(`The bill was ${bill}, the tip was ${tip}, and the total value is ${bill + tip}`);
+  
